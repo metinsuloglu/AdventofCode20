5b64df61034f63581497088a791d4ed6a9dd708c
Metin Suloglu <metinsuloglu97@gmail.com>
2020-12-15 11:57:01 +0000

    Improved Day 11

--- day11.py
+++ day11.py
@@ -21,7 +21,7 @@ with open('inputs/day11.txt') as layout:
     grid = np.array([[int(x) for x in list(r.translate(grid_converter))]
                      for r in layout.read().splitlines()])
 
-
+# Part 1
 kernel = np.array([[1,1,1],[1,0,1],[1,1,1]])
 curr_seats = np.copy(grid)
 while True:
@@ -33,33 +33,31 @@ while True:
                 
 part1 = np.count_nonzero(curr_seats == 2)
 
+# Part 2
+def closest_seat_coord(coord, offset):
+    curr_loc = (coord[0] + offset[0], coord[1] + offset[1])
+    while 0 <= curr_loc[0] < len(grid) and 0 <= curr_loc[1] < len(grid[curr_loc[0]]) and grid[curr_loc] == 0:
+        curr_loc = (curr_loc[0] + offset[0], curr_loc[1] + offset[1])
+    return curr_loc
+    
+directions = [(-1,-1),(-1,0),(-1,1),(0,-1),(0,1),(1,-1),(1,0),(1,1)]
+neighbours = np.array([[[closest_seat_coord((x, y), d) for d in directions]
+                        for y, c in enumerate(r)] for x, r in enumerate(grid)])
+padded_seats = np.zeros((grid.shape[0] + 2, grid.shape[1] + 2))
 
-kernel = np.zeros((3,3,8))
-kernel[(0,0,0,1,1,2,2,2), (0,1,2,0,2,0,1,2), range(8)] = 1.
-curr_seats = np.copy(grid)
-dp = np.zeros((curr_seats.shape[0] + 2, curr_seats.shape[1] + 2, 8))
 while True:
-    prev_seats = np.copy(curr_seats)
-    dp[1:-1, 1:-1, :] = curr_seats[..., None]
-    floor = np.where(curr_seats == 0)
-    
-    # forwards
-    for i,j in zip(*floor):
-        for idx, offset in enumerate([(-1,-1), (-1,0), (-1,1), (0,-1)]):
-            dp[i+1, j+1, idx] = dp[i+1 + offset[0], j+1 + offset[1], idx]
-    
-    # backwards
-    for i,j in zip(*[reversed(x) for x in floor]):
-        for idx, offset in enumerate([(0,1), (1,-1), (1,0), (1,1)]):
-            dp[i+1, j+1, idx+4] = dp[i+1 + offset[0], j+1 + offset[1], idx+4]
-            
-    res = convolve(np.where(dp[1:-1, 1:-1, :] == 2, 1, 0), kernel, mode='constant')[..., 3]
-    curr_seats[(curr_seats == 1) & (res == 0)] = 2
-    curr_seats[(curr_seats == 2) & (res >= 5)] = 1
-    if (prev_seats == curr_seats).all(): break
+    prev_seats = np.copy(grid)
+    padded_seats[1:-1, 1:-1] = grid
+    neighbour_vals = np.take(padded_seats,
+                             np.ravel_multi_index(np.rollaxis(neighbours + 1, -1),
+                                                  padded_seats.shape)
+                             )
+    res = np.sum(neighbour_vals == 2, axis=2)
+    grid[(grid == 1) & (res == 0)] = 2
+    grid[(grid == 2) & (res >= 5)] = 1
+    if (prev_seats == grid).all(): break
 
-part2 = np.count_nonzero(curr_seats == 2)
-
+part2 = np.count_nonzero(grid == 2)
 
 end = time.process_time()
 
